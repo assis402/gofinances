1aeb09f06fd51bb4337c3823df3f982aa692c328
Matheus de Assis <assis4002@gmail.com>
2022-10-27 22:32:20 -0300

    styled components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
-import { Container } from './styles'
+import { StyleSheet } from 'react-native'
+import { Container, Title } from './styles'
 
 export function DashBoard(){
     return (
         <Container>
-            <Text>Dashboard</Text>
+            <Title>Dashboard</Title>
         </Container>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    
-})
